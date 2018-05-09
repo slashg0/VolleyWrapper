91e073d33a8985ee9a03f785644302ecde32e227
SlashG <slashg0work@gmail.com>
2018-05-09 12:11:34 +0530

    [FEATURE] - Uses latest Volley Library - Added option to provide custom HurlStack - Added method to add simple Request without callback - Improved JavaDocs

--- release-notes.md
+++ release-notes.md
@@ -1,3 +1,10 @@
+# 0.2
+
+- [X] Uses latest Volley Library
+- [X] Added option to provide custom HurlStack
+- [X] Added method to add simple Request without callback
+- [X] Improved JavaDocs
+
 # 0.1
 
 - [X] Spine working
--- spine/src/main/java/xyz/slashg/spine/Spine.java
+++ spine/src/main/java/xyz/slashg/spine/Spine.java
@@ -1,9 +1,11 @@
 package xyz.slashg.spine;
 
 import android.content.Context;
+import android.util.Log;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.JsonRequest;
 import com.android.volley.toolbox.Volley;
 
@@ -11,196 +13,274 @@ import java.util.ArrayList;
 import java.util.Locale;
 
 /**
- *
  * ================
- *  Volley Wrapper
+ * Volley Wrapper
  * ================
- *
+ * <p>
  * This package contains a wrapper class structure for Volley requests
  * This provides simplified end points to queue HTTP requests in a single request queue
  * The methods can be statically accessed throughout the app importing it
- *
+ * <p>
  * Additional functionality of tags and priorities are provided for better control
- *
+ * <p>
  * --
  * Created by SlashG on 27-10-2015.
  */
 
-public class Spine {
+public class Spine
+{
 
-    public static final int PRIORITY_TIER_1 = 1, PRIORITY_TIER_2 = 2, PRIORITY_TIER_3 = 3;
-    private static ArrayList<String> tags;
-    private static RequestQueue requestQueue;
+	public static final int PRIORITY_TIER_1 = 1, PRIORITY_TIER_2 = 2, PRIORITY_TIER_3 = 3;
+	private static final String TAG = Spine.class.getSimpleName();
+	private static ArrayList<String> tags;
+	private static RequestQueue requestQueue;
+	private static boolean isLogEnabled = true;
+	private static boolean isStarted;
 
-    private static boolean isLogEnabled = true;
-    private static boolean isStarted;
+	/**
+	 * Method to initialize all elements of the Volley Wrapper Spine
+	 * All initializations are carried out here, without fail.
+	 * <p>
+	 *
+	 * @param context Context instance
+	 *                This ensures that no member object is null in the Spine
+	 */
 
-    public static void initialize(Context context) {
-        /**
-         * Method to initialize all elements of the Volley Wrapper Spine
-         * All initializations are carried out here, without fail.
-         *
-         * This ensures that no member object is null in the Spine
-         */
+	public static void initialize(Context context)
+	{
 
-        initTagList();                              //Initialize container ArrayList for tags
-        initJobQueue(context);                             //Initialize request queue
-        setIsStarted(false);                        //Initialize isStarted flag to false
-    }
+		initTagList();                              //Initialize container ArrayList for tags
+		initJobQueue(context);                             //Initialize request queue
+		setIsStarted(false);                        //Initialize isStarted flag to false
+	}
 
-    private static void initTagList() {
+	/**
+	 * Method to initialize all elements of the Volley Wrapper Spine
+	 * All initializations are carried out here, without fail.
+	 * <p>
+	 * This ensures that no member object is null in the Spine
+	 *
+	 * @param context   Context instance
+	 * @param hurlStack To allow custom {@code HurlStack} (to overcome error on OS below API19)
+	 */
+	public static void initialize(Context context, HurlStack hurlStack)
+	{
 
-        /**
-         * Initializes ArrayList of tags
-         */
 
-        tags = new ArrayList<>();
-    }
+		initTagList();                              //Initialize container ArrayList for tags
+		initJobQueue(context, hurlStack);                             //Initialize request queue
+		setIsStarted(false);                        //Initialize isStarted flag to false
+	}
 
-    private static void initJobQueue(Context context) {
+	/**
+	 * Initializes ArrayList of tags
+	 */
+	private static void initTagList()
+	{
 
-        /**
-         * Initializes requestQueue if necessary
-         */
 
-        requestQueue = Volley.newRequestQueue(context);
-    }
+		tags = new ArrayList<>();
+	}
 
-    private static void startRequests()
-    {
-        /**
-         * Method starts requestQueue whether or not it is running
-         */
+	/**
+	 * Initializes requestQueue if necessary
+	 * @param context   Context instance
+	 */
+	private static void initJobQueue(Context context)
+	{
 
-        requestQueue.start();
-        setIsStarted(true);
-    }
 
-    private static void stopAllRequests() {
+		requestQueue = Volley.newRequestQueue(context);
+	}
 
-        /**
-         * Method stops requestQueue if it is running
-         */
+	/**
+	 * Initializes requestQueue if necessary
+	 * @param context   Context instance
+	 * @param hurlStack To allow custom {@code HurlStack} (to overcome error on OS below API19)
+	 */
+	private static void initJobQueue(Context context, HurlStack hurlStack)
+	{
 
-        if (isStarted()) {
-            requestQueue.stop();
-            setIsStarted(false);
-        }
-    }
 
-    private static void startRequestsIfNeeded()
-    {
-        /**
-         * Method starts requestQueue if it isn't already running
-         */
+		requestQueue = Volley.newRequestQueue(context, hurlStack);
+	}
 
-        if(!isStarted()) {
-            startRequests();
-        }
-    }
+	/**
+	 * Method starts requestQueue whether or not it is running
+	 */
+	private static void startRequests()
+	{
 
-    public static void log(String message){
+		requestQueue.start();
+		setIsStarted(true);
+	}
 
-        /**
-         * This method is a standard method for logging events
-         *
-         * the log calls are discarded if boolean flag 'isLogEnabled' is false
-         */
+	/**
+	 * Method stops requestQueue if it is running
+	 */
+	private static void stopAllRequests()
+	{
 
-        if(isLogEnabled)
-        {
-            System.out.println(message);
-        }
-    }
 
-    public static void enableOrDisableLog(boolean enable)
-    {
-        /**
-         * Method sets value for flag 'isLogEnabled' to 'enable'
-         */
+		if (isStarted())
+		{
+			requestQueue.stop();
+			setIsStarted(false);
+		}
+	}
 
-        isLogEnabled = enable;
+	/**
+	 * Method starts requestQueue if it isn't already running
+	 */
+	private static void startRequestsIfNeeded()
+	{
 
-    }
 
-    public static boolean isStarted() {
+		if (!isStarted())
+		{
+			startRequests();
+		}
+	}
 
-        /**
-         * Method to check if the requestQueue is started or not
-         */
+	/**
+	 * This method is a standard method for logging events
+	 * <p>
+	 * the log calls are discarded if boolean flag 'isLogEnabled' is false
+	 *
+	 * @param message The message to log
+	 */
+	public static void log(String message)
+	{
 
-        return isStarted;
-    }
 
-    private static void setIsStarted(boolean isStarted) {
+		if (isLogEnabled)
+		{
+			Log.d(TAG, "log: " + message);
+		}
+	}
 
-        /**
-         * Method sets isStarted flag to the passed value
-         */
+	/**
+	 * Method sets value for flag 'isLogEnabled' to 'enable'
+	 *
+	 * @param enable The value to set
+	 */
+	public static void enableOrDisableLog(boolean enable)
+	{
 
-        Spine.isStarted = isStarted;
-    }
+		isLogEnabled = enable;
 
-    public static boolean registerTag(String tag) {
+	}
 
-        /**
-         * Method to register a new tag if it doesn't already exist.
-         * All tags are invariably in uppe-case
-         *
-         * If a tag is already there in the list, this method returns false
-         *
-         * NOTE: Tags are an optional parameter that can provide greater grip on the requests
-         */
+	/**
+	 * Method to check if the requestQueue is started or not
+	 * @return Whether Spine is started or not
+	 */
+	public static boolean isStarted()
+	{
 
-        if (tag != null && tag.length() > 0) {   // Check the validity of the input
 
-            String tagUpperCase = tag.toUpperCase(Locale.US);
-            if (tags.contains(tagUpperCase)) {
-                // Check if the tag already exists
-                return false;
-            }
+		return isStarted;
+	}
 
-            tags.add(tagUpperCase);
-        }
-        return true;
-    }
+	/**
+	 * Method sets isStarted flag to the passed value
+	 *
+	 * @param isStarted The value to set
+	 */
+	private static void setIsStarted(boolean isStarted)
+	{
 
-    public static void queueRequest(EasyRequest newRequest)
-    {
-        /**
-         * Method used to add requests to the request queue
-         */
-        JsonRequest req = newRequest.getProcessedRequest();
-        if(req != null) {
-            requestQueue.add(newRequest.getProcessedRequest());
-            startRequestsIfNeeded();
 
-        }
-        else
-        {
-            log("Spine::queueRequest() processed request is null");
-        }
-    }
+		Spine.isStarted = isStarted;
+	}
 
-    public static void queueRequest(final Request newRequest, final Runnable requestCompleteAction)
-    {
-        /**
-         * Method used to add requests to the request queue
-         * and register callback
-         */
-        requestQueue.add(newRequest);
-        requestQueue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<Object>() {
-            @Override
-            public void onRequestFinished(Request<Object> request) {
-                if (request.equals(newRequest)) {
-                    /**
-                     * When the request queued in this call is completed, execute requestCompleteAction
-                     */
-                    requestCompleteAction.run();
-                }
-            }
-        });
-        startRequestsIfNeeded();
-    }
+	/**
+	 * Method to register a new tag if it doesn't already exist.
+	 * All tags are invariably in upper-case
+	 * <p>
+	 * NOTE: Tags are an optional parameter that can provide greater grip on the requests
+	 *
+	 * @param tag The string to register if not already registered. Will be forced to Upper Case
+	 * @return 'true' if tag successfully registered. If  the tag is already there in the list, this method returns 'false'.
+	 */
+	public static boolean registerTag(String tag)
+	{
+
+
+		if (tag != null && tag.length() > 0)
+		{   // Check the validity of the input
+
+			String tagUpperCase = tag.toUpperCase(Locale.US);
+			if (tags.contains(tagUpperCase))
+			{
+				// Check if the tag already exists
+				return false;
+			}
+
+			tags.add(tagUpperCase);
+		}
+		return true;
+	}
+
+	/**
+	 * Method used to an {@link EasyRequest} to the request queue
+	 *
+	 * @param newRequest The request to queue
+	 */
+	public static void queueRequest(EasyRequest newRequest)
+	{
+		JsonRequest req = newRequest.getProcessedRequest();
+		if (req != null)
+		{
+			requestQueue.add(newRequest.getProcessedRequest());
+			startRequestsIfNeeded();
+
+		}
+		else
+		{
+			log("Spine::queueRequest() processed request is null");
+		}
+	}
+
+	/**
+	 * Method used to add requests to the request queue
+	 * and register callback
+	 *
+	 * @param newRequest            The request to queue
+	 * @param requestCompleteAction A simple no-parameter callback to notify completion of the request. This is apart from the callback registered in newRequest itself
+	 */
+	public static void queueRequest(final Request<Object> newRequest, final Runnable requestCompleteAction)
+	{
+
+		requestQueue.add(newRequest);
+		requestQueue.addRequestFinishedListener(new RequestQueue.RequestFinishedListener<Object>()
+		{
+			@Override
+			public void onRequestFinished(Request<Object> request)
+			{
+				if (request.equals(newRequest))
+				{
+					/**
+					 * When the request queued in this call is completed, execute requestCompleteAction
+					 */
+					requestCompleteAction.run();
+				}
+			}
+		});
+		startRequestsIfNeeded();
+	}
+
+	/**
+	 * Method used to add requests to the request queue
+	 * without registering a callback
+	 *
+	 * @param newRequest The request to queue
+	 */
+	public static void queueRequest(final Request<Object> newRequest)
+	{
+
+		requestQueue.add(newRequest);
+		startRequestsIfNeeded();
+	}
 
 }
